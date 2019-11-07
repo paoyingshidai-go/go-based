0dbbf69c56ddc7581c6140b6476add3abf8c98db
Michael <13432849094@163.com>
2019-11-07 10:09:23 +0800

    添加线程安全的 map

--- mymap/mymap.go
+++ mymap/mymap.go
@@ -1,6 +1,73 @@
-package mymap
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	ValueOrPoint()
+
+	ThreadSafe2()
+
+}
+
+// map 是线程不安全的
+func ThreadSafe() {
+	//c := make(map[string]int)
+	c := make(map[string]int)
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				c[fmt.Sprintf("%d", j)] = j
+			}
+		}()
+	}
+	time.Sleep(3 * time.Second)
+	fmt.Println(c)
+}
+
+// https://blog.csdn.net/qq_39920531/article/details/88062841
+func ThreadSafe2() {
+	//开箱即用
+	var sm sync.Map
+	//store 方法,添加元素
+	sm.Store(1, "a")
+	//Load 方法，获得value
+	if v, ok := sm.Load(1); ok {
+		fmt.Println(v)
+	}
+	//LoadOrStore方法，获取或者保存
+	//参数是一对key：value，如果该key存在且没有被标记删除则返回原先的value（不更新）和true；不存在则store，返回该value 和false
+	if vv, ok := sm.LoadOrStore(1, "c"); ok {
+		fmt.Println(vv)
+	}
+	if vv, ok := sm.LoadOrStore(2, "c"); !ok {
+		fmt.Println(vv)
+	}
+	//遍历该map，参数是个函数，该函数参的两个参数是遍历获得的key和value，返回一个bool值，当返回false时，遍历立刻结束。
+	sm.Range(func(k, v interface{}) bool {
+		fmt.Print(k)
+		fmt.Print(":")
+		fmt.Print(v)
+		fmt.Println()
+		return true
+	})
+}
+
+// 值传递还是引用传递
+func ValueOrPoint() {
+	m := map[string]int{"value": 0}
+	m1 := m
+	fmt.Println("m =", m)
+	fmt.Println("m1 =", m1)
+	m1["value"] = 1
+	fmt.Println("m =", m)
+	fmt.Println("m1 =", m1)
+}
 
 func SetAndPrintMap() {
 	var countryCapitalMap map[string]string /*创建集合 */
